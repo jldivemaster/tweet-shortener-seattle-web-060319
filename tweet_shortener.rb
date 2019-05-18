3bf3177549652b5ffe307c9ff6359571569b67d8
Jason Leach <jlspursfan@gmail.com>
2019-05-18 15:19:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,7 +36,7 @@ def bulk_tweet_shortener(array)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.size > 140
+  if tweet.length > 140
     puts word_substituter(tweet)
   else
     puts tweet
